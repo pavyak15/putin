b5fbb55318438fe88393de8d42885576bad34832
pavyak15 <92105549+pavyak15@users.noreply.github.com>
2021-10-14 18:23:48 +0300

    Create 2-12.py

---
+++ laba2/2-12.py
@@ -0,0 +1,10 @@
+n = int(input())
+import turtle
+turtle.shape('turtle')
+turtle.speed(10)
+turtle.lt(90)
+def vitok():
+    turtle.circle(25, 180)
+    turtle.circle(5,180)
+for i in range(n):
+    vitok()
